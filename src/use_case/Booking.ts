0cea6cac76f46480484da039499acd3a7d9cf6dc
melon <momobp114@gmail.com>
2023-01-28 17:57:41 +0700

    Fix isOverlapDate logic

--- src/use_case/Booking.ts
+++ src/use_case/Booking.ts
@@ -1,6 +1,9 @@
 import Booking from "../entities/Booking";
 import BookingRepository from "../repository/BookingRepository";
+import PostRepository from "../repository/PostRepository";
 import { generateBookingId } from "../utils/generateId";
+import MockBookingRepository from "./tests/mocks/MockBookingRepository";
+import MockPostRepository from "./tests/mocks/MockPostRepository";
 
 export type BookingRequest = {
   clientId: string
@@ -11,16 +14,29 @@ export type BookingRequest = {
 
 class BookingUseCase {
   bookingRepository: BookingRepository
+  postRepository: PostRepository
 
-  constructor(bookingRepository: BookingRepository) {
+  constructor(bookingRepository: BookingRepository | MockBookingRepository, postRepository: PostRepository | MockPostRepository) {
     this.bookingRepository = bookingRepository
+    this.postRepository = postRepository
   }
 
-  createBooking(bookingRequest: BookingRequest) {
+  async createBooking(bookingRequest: BookingRequest) {
     // validation phase
-    const bookingList = this.bookingRepository.getAll()
+    const post = await this.postRepository.getById(bookingRequest.postId)
+    if(post == null) {
+      throw new Error(`Invalid post id ${bookingRequest.postId} doesn't exist`)
+    }
+
+    if(bookingRequest.from > bookingRequest.to) {
+      throw new Error(`Invalid start date and finish date`)
+    }
+
+    const bookingList = await this.bookingRepository.getAll()
     const isBooked = bookingList.some(booking => {
-      return isOverlapDate(bookingRequest.from, bookingRequest.to, booking.from, booking.to) && (booking.status == "booked" || booking.status == "check-in")
+      return booking.postId === bookingRequest.postId 
+      && isOverlapDate(booking.from, booking.to, bookingRequest.from, bookingRequest.to) 
+      && (booking.status == "booked" || booking.status == "check-in")
     })
     if (isBooked) {
       throw new Error(`Post Id ${bookingRequest.postId} has been booked`)
@@ -39,8 +55,8 @@ class BookingUseCase {
     this.bookingRepository.create(booking)
   }
 
-  cancelBooking(id: string) {
-    const booking = this.bookingRepository.getById(id)
+  async cancelBooking(id: string) {
+    const booking = await this.bookingRepository.getById(id)
     if(booking == undefined) {
       throw new Error(`Booking Id ${id} not found`)
     }
@@ -55,16 +71,19 @@ class BookingUseCase {
 }
 
 const isOverlapDate = (s_from: Date, s_to: Date, d_from: Date, d_to: Date) => {
-  if(d_to > s_from && d_to < s_to) {
+  if((d_to > s_from && d_to < s_to) || (s_to > d_from && s_to < d_to)) {
     return true
   }
-  if(d_from > s_from && d_from < s_to) {
+  if((d_from > s_from && d_from < s_to) || (s_from > d_from && s_from < d_to)) {
     return true
   }
   if(d_from < s_from && d_to > s_to) {
     return true
   }
   if(d_from > s_from && d_to < s_to) {
+    return true
+  }
+  if(d_from == s_from && d_to == s_to) {
     return true
   }
 
